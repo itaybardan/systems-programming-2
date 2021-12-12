db32a71ddbaa1a22c0220fe0408032bfb62c3bba
ziintoth <95305209+ziintoth@users.noreply.github.com>
2021-12-12 15:44:13 +0200

    Update Student.java
    Added incrementing for publifications and papersRead fields.
    *note: papersRead includes all papers received from the conferences excluding the specific student's papers(models), the amount received in the increment method will be calculated in advance, inside StudentService.

--- Student.java
+++ Student.java
@@ -1,6 +1,7 @@
 package bgu.spl.mics.application.objects;
 
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Passive object representing single student.
@@ -17,17 +18,24 @@ public class Student {
     private int name;
     private String department;
     private Degree status;
-    private int publications;
-    private int papersRead;
+    private AtomicInteger publications; //these values need to be updates instantly.
+    private AtomicInteger papersRead;
     private ArrayList<Model> models;
 
     public Student(int _name, String _department, Degree _status, ArrayList<Model> _models) {
         name = _name;
         department = _department;
         status = _status;
-        publications = 0;
-        papersRead = 0;
+        publications = new AtomicInteger(0);
+        papersRead = new AtomicInteger(0); // All papers publicized except papers that belong to this Student.
         models = _models;
     }
-    
+
+    public void incrementPublifications(){
+        publications.set(publications.get()+1);
+    }
+    public void incrementPapersRead(int amount){ //The amount excluding this student's publications will be calculated in service
+        papersRead.set(papersRead.get()+amount);
+    }
+
 }
