8a2e4c97a7e0373b79d5886ba4da3d5b828c4e2a
ziintoth <95305209+ziintoth@users.noreply.github.com>
2021-12-16 00:46:26 +0200

    Update MainTest.java
    Updated MainTest with implementation of TimeService, StudentService and ConferenceService (with a fake gpuservice)

--- src/test/java/bgu/spl/mics/MainTest.java
+++ src/test/java/bgu/spl/mics/MainTest.java
@@ -1,38 +1,90 @@
 package bgu.spl.mics;
 
-import bgu.spl.mics.application.objects.ConferenceInformation;
-import bgu.spl.mics.application.objects.Model;
-import bgu.spl.mics.application.objects.ModelType;
-import bgu.spl.mics.application.objects.Student;
+import bgu.spl.mics.application.broadcasts.PublishConferenceBroadcast;
+import bgu.spl.mics.application.events.TestModelEvent;
+import bgu.spl.mics.application.events.TrainModelEvent;
+import bgu.spl.mics.application.objects.*;
 import bgu.spl.mics.application.services.ConferenceService;
 import bgu.spl.mics.application.services.StudentService;
 import bgu.spl.mics.application.services.TimeService;
 import org.junit.Before;
 import org.junit.Test;
 
+
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 
+
+class ExampleGPU extends MicroService{
+
+
+    /**
+     * @param name the micro-service name (used mainly for debugging purposes -
+     *             does not have to be unique)
+     */
+    public ExampleGPU(String name) {
+        super(name);
+
+    }
+
+    @Override
+    protected void initialize() {
+
+        //Setting up Callbacks
+        Callback<TrainModelEvent> trainModelEventCallback = (TrainModelEvent e) -> {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            System.out.println("gpu on train");
+            complete(e, e.getModel());
+        };
+        Callback<TestModelEvent> testModelEventCallback = (TestModelEvent e) -> {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            System.out.println("gpu on test");
+            Random random = new Random();
+            double randNum = random.nextDouble();
+            ModelStatus modelStatus = ModelStatus.Bad;
+            if(randNum >= 0.6) modelStatus = ModelStatus.Good;
+            complete(e, modelStatus);
+
+        };
+
+        subscribeEvent(TrainModelEvent.class, trainModelEventCallback);
+        subscribeEvent(TestModelEvent.class, testModelEventCallback);
+
+    }
+}
+
+
 public class MainTest {
 
-    final int ticks = 1;
-    final int duration = 300;
-    bgu.spl.mics.example.ExampleMicroService exampleMicroService;
+    final int ticks=1;
+    final int duration=600;
+    ExampleMicroService exampleMicroService;
     StudentService studentService;
     ConferenceService conferenceService1, conferenceService2;
+    ExampleGPU exampleGPU;
     TimeService timeService;
+    MessageBusImpl messageBus;
 
 
     @Before
-    public void beforeTest() {
+    public void beforeTest(){
 
         ArrayList<Model> models = new ArrayList<Model>();
-        Model m1 = new Model("test1", ModelType.images, 10000);
-        Model m2 = new Model("test2", ModelType.tabular, 20000);
-        Model m3 = new Model("test3", ModelType.text, 30000);
+        Model m1 = new Model("test1", ModelType.Images, 10000);
+        Model m2 = new Model("test2", ModelType.Tabular, 20000);
+        Model m3 = new Model("test3", ModelType.Text, 30000);
         models.add(m1);
         models.add(m2);
         models.add(m3);
@@ -40,26 +92,47 @@ public class MainTest {
 
         Student student1 = new Student("stud1", "dep", Student.Degree.MSc, models);
         ConferenceInformation conf1 = new ConferenceInformation("conf1", 50);
-        ConferenceInformation conf2 = new ConferenceInformation("conf1", 70);
+        ConferenceInformation conf2 = new ConferenceInformation("conf1", 450);
 
-        exampleMicroService = new bgu.spl.mics.example.ExampleMicroService("exm");
+        exampleMicroService = new ExampleMicroService("exm");
+        exampleGPU = new ExampleGPU("gpu");
         studentService = new StudentService(student1);
         int confCounter = 1;
         conferenceService1 = new ConferenceService("conf1", conf1, confCounter);
-        confCounter = conf1.getDate();
+        confCounter=conf1.getDate();
         conferenceService2 = new ConferenceService("conf2", conf2, confCounter);
 
+
+        messageBus = MessageBusImpl.getInstance();
     }
 
     @Test
     public void runTest() {
 
         ExecutorService fixedPool = Executors.newFixedThreadPool(5);
-        timeService = new TimeService("time-service-test", ticks, duration);
+        timeService = new TimeService(ticks, duration);
+        fixedPool.execute(timeService);
+        fixedPool.execute(exampleGPU);
         fixedPool.execute(conferenceService1);
         fixedPool.execute(conferenceService2);
+        
+        Thread thread = new Thread ( () -> { // Need to have some sort of delay between setup of gpus and student services
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         fixedPool.execute(studentService);
-        fixedPool.execute(timeService);
+
+
 
         fixedPool.shutdown();
         try {
@@ -67,6 +140,7 @@ public class MainTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
 
     }
 
