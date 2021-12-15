3d4061555440791832e6437b87864831e2da80c5
ziintoth <95305209+ziintoth@users.noreply.github.com>
2021-12-16 01:16:51 +0200

    Update PublishResultsEvent.java
    The conference receives the model and adds it to it's array.

--- src/main/java/bgu/spl/mics/application/messages/PublishResultsEvent.java
+++ src/main/java/bgu/spl/mics/application/messages/PublishResultsEvent.java
@@ -1,12 +1,20 @@
-package bgu.spl.mics.application.messages;
+package bgu.spl.mics.application.events;
 import bgu.spl.mics.Event;
 import bgu.spl.mics.Future;
+import bgu.spl.mics.application.objects.Model;
 
 public class PublishResultsEvent implements Event<String> {
-    public PublishResultsEvent(){
-        super();
+    private Model model;
 
+    public PublishResultsEvent(Model _model){ //Conference will receive this event and increment needed values to the student
+        super();
+        model = _model;
     }
+
+    public Model getModel(){
+        return model;
+    }
+
     @Override
     public Future<String> getFuture() {
         return null;
